d09882bc04bcf806082bd284f44d0af73c01f58d
Jonathan Houtmeyers <Houtmeyers Jonathan>
2024-12-18 09:57:53 +0100

    setup done

---
+++ src/app/components/control/Control.jsx
@@ -0,0 +1,6 @@
+const Control = () => {
+    return (
+        <div>Control</div>
+    )
+}
+export default Control
---
+++ src/app/components/tree/Tree.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+const Tree = () => {
+    return (
+        <div>
+            <Image src="/images/tree.png" alt="Christmas Tree" width={500} height={500} />
+        </div>
+    )
+}
+export default Tree 
